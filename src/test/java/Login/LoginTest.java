678dd5b3de0746dbf20cd040e9fc9026ec2823b2
Ashawami Mulik <ashawami4@gmail.com>
2021-06-23 14:55:34 +0530

    Added Env Variables in login test

--- src/test/java/Login/LoginTest.java
+++ src/test/java/Login/LoginTest.java
@@ -1,18 +1,22 @@
 package Login;
 
+import com.relevantcodes.extentreports.LogStatus;
 import testsetup.BaseTest;
 import org.testng.annotations.*;
 import pages.Login;
 
+import static java.lang.System.getenv;
 import static org.junit.Assert.assertEquals;
 
 public class LoginTest extends BaseTest {
     @Test
     public void testLoginAndLogout() {
+        gt = gr.startTest("testLoginAndLogout");
         Login login = new Login(driver);
         login.clickLoginButton();
-        login.as(prop.getProperty("correctEmail"), prop.getProperty("correctPassword"));
+        login.as(getenv("EMAIL") , getenv("PASSWORD"));
         login.clickLogoutButton();
+        gt.log(LogStatus.PASS, "Test Passed");
     }
 
     @Test
